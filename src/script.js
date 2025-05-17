e1966b91e7e235353184f3c4eab23471035697b2
nmcsakib <nmcsakib.1@gmail.com>
2025-05-17 16:42:58 +0600

    added funtional card

--- src/script.js
+++ src/script.js
@@ -1,1 +1,23 @@
-console.log('hello world');
+function updateBalance(button) {
+    console.log('updateBalance function called');
+    const card = button.closest('.flex-1');
+    console.log(card.querySelector('#donation-value'));
+    let balance = parseFloat(document.getElementById('balance').innerText);
+    const donationValue = parseFloat(card.querySelector('#donation-value').value);
+    let donationAmount = parseFloat(card.querySelector('#donation-amount').innerText);
+    if(donationValue <= balance){
+        if(donationValue > 0){
+            document.getElementById('balance').innerText = balance - donationValue;
+            card.querySelector('#donation-amount').innerText = donationAmount+donationValue;
+            console.log("Balance updated to: ", balance);
+        }
+        else{
+            alert("Please enter a valid donation amount");
+        }
+    } else{
+        alert("Insufficient balance");
+    }
+    card.querySelector('#donation-value').value = '';
+
+    
+}
